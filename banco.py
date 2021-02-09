0705db1ec522d7df90f0ff9285a0b171d215b7aa
dennis310797 <dennis0201@gmail.com>
2021-02-09 19:02:38 -0300

    add arquivos

---
+++ banco.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+conector = sqlite3.connect("dbCadastro.db")
+
+cursor = conector.cursor()
+
+#cursor.execute("CREATE TABLE pessoas (nome varchar, email varchar, telefone int)")
+
+class cadastro:
+    def __init__ (self, nome, email, telefone):
+        self.nome=nome
+        self.email=email
+        self.telefone=telefone
+        
+    def insert (self):
+        cursor.execute("INSERT INTO pessoas VALUES ('"+str(self.nome)+"','"+str(self.email)+"','"+str(self.telefone)+"')")
+        conector.commit()
+        
+    def getData (self):
+        cursor.execute("SELECT * FROM pessoas")
+        dataRead = cursor.fetchall()
+        return dataRead
---
+++ main.py
@@ -0,0 +1,35 @@
+from PyQt5 import uic, QtWidgets
+import banco as db
+
+def showDB(obj):
+    data=obj.getData()
+    screem.tableWidget.setRowCount(len(data))
+    screem.tableWidget.setColumnCount(3)
+    
+    for i in range(0,len(data)):
+        for j in range (0,3):
+            screem.tableWidget.setItem(i,j,QtWidgets.QTableWidgetIte.m9`str(data[i[j]]))
+def main():
+    
+    nome=""
+    email=""
+    telefone=0
+    
+    nome = screem.lineEdit.text()
+    email = screem.lineEdit_2.text()
+    telefone = screem.lineEdit_3.text()
+    
+    obj = db.cadastro(nome, email, telefone)
+    obj.insert()
+    showDB(obj) 
+    
+    
+def raffle():
+    print("olá mundo")
+
+app=QtWidgets.QApplication([])
+screem=uic.loadUi("main.ui")
+screem.pushButton.clicked.connect(main)
+screem.pushButton_2.clicked.connect(raffle)
+screem.show()
+app.exec()
